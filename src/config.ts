c4989f7079ff0e801aefecd6803cb864ac7e96d5
joon9823 <joon9823@gmail.com>
2024-02-22 14:32:27 +0900

    Remove unused and change port

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ dotenv.config()
 const { SERVER_PORT, MONITOR_INTERVAL, LCD_URL, RPC_URL } = process.env
 
 export const config = {
-  SERVER_PORT: SERVER_PORT ? parseInt(SERVER_PORT) : 6000,
+  SERVER_PORT: SERVER_PORT ? parseInt(SERVER_PORT) : 3000,
   MONITOR_INTERVAL: MONITOR_INTERVAL ? parseInt(MONITOR_INTERVAL) : 100,
   LCD_URL: LCD_URL ? LCD_URL.split(',') : ['http://localhost:1317'],
   RPC_URL: RPC_URL ? RPC_URL.split(',') : ['http://localhost:26657'],
